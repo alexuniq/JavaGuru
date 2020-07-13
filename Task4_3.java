7ab1d4535f52e53f815e9fe1fafca90ac0b3a711
Alex <alexuniq@inbox.lv>
2020-07-13 22:20:47 +0300

    Add files via upload

---
+++ Task4_3.java
@@ -0,0 +1,17 @@
+package samples.l1;
+
+public class Task4_3 {
+
+        public static void main(String[] args) {
+            int a = 74;
+            int b = 36;
+            int c = a + b;
+            int d = 50;
+            int e = 3;
+            int f = d / e;
+
+            System.out.println(c);
+            System.out.println(f);
+
+    }
+}
